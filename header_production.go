4b3e0b4f5f39e3f51b391c7b745f2c546dfc1f17
manhkim <manhtokim@gmail.com>
2024-01-24 21:18:11 +0700

    final

--- header_production.go
+++ header_production.go
@@ -3,7 +3,7 @@ package main
 // @version     2.0
 // @description APIs list for project PNK-Intern
 
-// @host    localhost:12002
+// @host   	 pheniaa-intern.linkdevcraft.online
 // @BasePath /api/v1
 
 // @securityDefinitions.apikey ApiKeyAuth
